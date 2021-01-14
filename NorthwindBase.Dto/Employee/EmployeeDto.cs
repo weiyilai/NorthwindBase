dc0e19ace9293ad9e72ed29477c0896231d2227e
stevenlai <stevenlai@urtoday.com>
2021-01-14 15:01:17 +0800

    delete summary

--- NorthwindBase.Dto/Employee/EmployeeDto.cs
+++ NorthwindBase.Dto/Employee/EmployeeDto.cs
@@ -11,33 +11,12 @@ namespace NorthwindBase.Dto.Employee
     /// </summary>
     public class EmployeeDto
     {
-        /// <summary>
-        /// 員工編號
-        /// </summary>
         public int EmployeeID { get; set; }
-        /// <summary>
-        /// 員工名字
-        /// </summary>
         public string LastName { get; set; }
-        /// <summary>
-        /// 員工姓氏
-        /// </summary>
         public string FirstName { get; set; }
-        /// <summary>
-        /// 抬頭
-        /// </summary>
         public string Title { get; set; }
-        /// <summary>
-        /// 稱謂
-        /// </summary>
         public string TitleOfCourtesy { get; set; }
-        /// <summary>
-        /// 生日
-        /// </summary>
         public DateTime? BirthDate { get; set; }
-        /// <summary>
-        /// 僱用日期
-        /// </summary>
         public DateTime? HireDate { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
